fa8818005bee28dd8dc5ff3dc4c24fd79601de81
Rupesh Mangal <rupesh.code@gmail.com>
2018-02-08 18:31:23 +0530

    Technology page

--- technology/views.py
+++ technology/views.py
@@ -6,9 +6,11 @@ from course.models import Course
 
 def technology(request,tech):
     try:
-        tech = Technology.objects.filter(title=tech)
+        tech = Technology.objects.get(title=tech)
     except Technology.DoesNotExist:
         tech = None
-    # try:
-    #     courses = Course.objects.filter(tech=tech)
-    return render(request, 'technology.html', {'tech': tech})
+    try:
+         courses = Course.objects.filter(tech=tech)
+    except Course.DoesNotExist:
+        courses = None
+    return render(request, 'technology.html', {'tech': tech, 'courses':courses})
